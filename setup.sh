83bae1a398daa33890ede68c24147fd9a6fa019f
Polarstingray <88648866+Polarstingray@users.noreply.github.com>
2026-03-31 12:52:55 -0500

    Refactor setup.sh
    Refactor setup script to improve error handling and simplify directory navigation.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
+set -euo pipefail
 echo "setting up affirm..."
-curr=$(pwd)
 
-in_affirm=$(echo $curr | grep "affirm")
-if [ $? -ne 0 ]; then
-    cd "$curr/affirm"
-    if [ $? -ne 0 ]; then
-        echo "unable to find affirm directory"
-        echo "Try running the setup.sh in the /affirm directory"
-        exit 1
-    fi
-fi
+JOB_DIR="${1:-$(cd "$(dirname "$0")" && pwd)}"
+cd "$JOB_DIR"
 
 echo "cleaning..."
 make clean
@@ -21,13 +13,8 @@ echo "creating directories and installing binary..."
 make install
 
 BIN_DIR="$HOME/.local/bin"
-in_path=$(echo $PATH | grep "$BIN_DIR")
-if [ $? -ne 0 ]; then
+if ! echo "$PATH" | grep -q "$BIN_DIR"; then
     echo "adding $BIN_DIR to $HOME/.bashrc"
-    echo "export PATH=\"$PATH:$BIN_DIR\"" >> $HOME/.bashrc
+    echo "export PATH=\"$PATH:$BIN_DIR\"" >> "$HOME/.bashrc"
     export PATH="$PATH:$BIN_DIR"
 fi
-
-cd $curr
-
-echo "Done installing affirm."
